e448163c71c4d48ed7aa77248a4df8c9092a05ce
Vtluan191123 <Vtluan191123@gamil.com>
2024-05-27 22:02:59 +0700

    update file

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello controller with vu thanh luan";
+        return "Hello controller 111`";
+    }
+
+    @GetMapping("/user")
+    public String GetUserPage() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String GetAdminPage() {
+        return "admin`";
     }
 
 }
